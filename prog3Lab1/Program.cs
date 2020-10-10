c09428c0e70a3a74358aa3f70f4a43a5f221120d
= <szysz@outlook.com>
2020-10-10 23:49:58 +0200

    Added new functionality

--- prog3Lab1/Program.cs
+++ prog3Lab1/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace prog3Lab1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("HEWWOOO");
+namespace prog3Lab1 {
+    public enum Mz {
+        Dwa,
+        Jedno,
+        Brak
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            double a = 0, b = 0, c = 0;
+
+            if (!double.TryParse(Console.ReadLine(), out a) ||
+                !double.TryParse(Console.ReadLine(), out b) ||
+                !double.TryParse(Console.ReadLine(), out c)) {
+                Console.WriteLine("Podano niepoprawne dane");
+                return;
+            }
+
+            double delta = (Math.Pow(b, 2) - 4 * a * c);
+            Console.WriteLine($"Delta: {delta}");
+            Mz iloscMiejsc;
+
+            if (delta > 0) {
+                iloscMiejsc = Mz.Dwa;
+            }
+            else if (delta == 0) {
+                iloscMiejsc = Mz.Jedno;
+            }
+            else {
+                iloscMiejsc = Mz.Brak;
+            }
+
+            Console.WriteLine($"Ilość miejsc zerowych w tej delcie to: {iloscMiejsc}");
+
+            switch (iloscMiejsc) {
+                case Mz.Dwa:
+                    Console.WriteLine($"x1: {((-b + Math.Sqrt(delta)) / 2 * a)}" +
+                                      $"\nx2: {((-b - Math.Sqrt(delta)) / 2 * a)}");
+                    break;
+
+                case Mz.Jedno:
+                    Console.WriteLine($"x1: {(-b / 2 * a)}");
+                    break;
+            }
         }
     }
 }
